8b6dce6b585830735d07e10e392dee667043fe78
Aaradhya Rai <ar-aaradhyarai@gmail.com>
2020-12-02 13:54:02 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,10 @@
+// console.log("script is working from different file!");
+
+
+var userName=prompt("Give your username")
+var welcomeMessage = "this script works!!" + userName
+alert(welcomeMessage);
+// alert("This script works " + userName);
+
+
+
